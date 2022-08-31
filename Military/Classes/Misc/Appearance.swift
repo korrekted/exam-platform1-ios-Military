1d93bf17ecea7526b328e6097180a58ca738a4f5
akonst17 <akonst17@gmail.com>
2022-08-31 17:40:55 +0300

    вординг; build 17

--- Military/Classes/Misc/Appearance.swift
+++ Military/Classes/Misc/Appearance.swift
@@ -15,7 +15,7 @@ struct Appearance {
     static let successColor = UIColor(integralRed: 46, green: 190, blue: 161)
     static let errorColor = UIColor(integralRed: 255, green: 115, blue: 99)
     static let warningColor = UIColor(integralRed: 255, green: 155, blue: 38)
-    static let backgroundColor = UIColor(integralRed: 255, green: 254, blue: 247)
+    static let backgroundColor = UIColor(integralRed: 247, green: 248, blue: 246)
     static let greyColor = UIColor(integralRed: 75, green: 81, blue: 102)
     static let blackColor = UIColor(integralRed: 19, green: 24, blue: 42)
 }
