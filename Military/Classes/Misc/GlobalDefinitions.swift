2aaba26f94ec6a697c85b24c303b2a915204cc26
akonst17 <akonst17@gmail.com>
2022-09-19 21:10:03 +0300

    интеграция sdk 2.0; новые урлы; build 18

--- Military/Classes/Misc/GlobalDefinitions.swift
+++ Military/Classes/Misc/GlobalDefinitions.swift
@@ -6,21 +6,24 @@
 //
 
 final class GlobalDefinitions {
-    static let domainUrl = "https://exams2.quantresear.ch"
+    static let domainUrl = "https://exams2.mastrapi.com"
     static let apiKey = "PkS5dosf76N3aR9B"
-
-    static let sdkDomainUrl = "https://analytics.korrekted.com"
-    static let sdkApiKey = "hQ3ZPuVzqM8X1obF"
 
     static let applicationTag = "EXAM_MILITARY"
 
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
+    
+    static let sdkDomainUrl = "https://analytics.mastrapi.com"
+    static let sdkApiKey = "hQ3ZPuVzqM8X1obF"
+    
+    static let otterScaleHost = "https://analytics2.mastrapi.com"
+    static let otterScaleApiKey = "IsXWqhosNdWE7B42"
 
     static let appStoreUrl = "https://apps.apple.com/app/asvab-practice-test-2022/id1561487785"
     static let appleAppID = "1561487785"
 
-    static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
-    static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
-    static let contactUsUrl = "https://quantresear.ch/contact.html"
+    static let termsOfServiceUrl = "https://mastrapi.com/terms"
+    static let privacyPolicyUrl = "https://mastrapi.com/policy"
+    static let contactUsUrl = "https://mastrapi.com/contact"
 }
